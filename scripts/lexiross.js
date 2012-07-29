b799e4af429a6bf56ca27ebc982636af59413b98
Lexi Ross <the.circ@gmail.com>
2012-07-28 21:20:34 -0400

    added effect to all three sections

--- scripts/lexiross.js
+++ scripts/lexiross.js
@@ -10,6 +10,34 @@ jQuery(document).ready(function() {
             jQuery("#about-plus").html("-");
         }
     });
+
+    
+    jQuery("#projects-heading").click(function() {
+        if (jQuery("#projects").hasClass("visible")) {
+            jQuery("#projects").slideUp();
+            jQuery("#projects").removeClass("visible");
+            jQuery("#projects-plus").html("+");
+        } else {
+            jQuery("#projects").slideDown();
+            jQuery("#projects").addClass("visible");
+            jQuery("#projects-plus").html("-");
+        }
+    });
+
+
+    jQuery("#contact-heading").click(function() {
+        if (jQuery("#contact").hasClass("visible")) {
+            jQuery("#contact").slideUp();
+            jQuery("#contact").removeClass("visible");
+            jQuery("#contact-plus").html("+");
+        } else {
+            jQuery("#contact").slideDown();
+            jQuery("#contact").addClass("visible");
+            jQuery("#contact-plus").html("-");
+        }
+    });
 });
+
+
 
 //TODO: clean up scriptaculous files
