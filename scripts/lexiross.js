d6b0323efbcff9ba9bac42c0c9c49c9a0aa2b6d8
Lexi Ross <the.circ@gmail.com>
2012-07-28 21:15:28 -0400

    replaced scriptaculous effect with jquery

--- scripts/lexiross.js
+++ scripts/lexiross.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function() {
     jQuery("#about-heading").click(function() {
         if (jQuery("#about").hasClass("visible")) {
-            jQuery("#about").blindUp();
+            jQuery("#about").slideUp();
             jQuery("#about").removeClass("visible");
+            jQuery("#about-plus").html("+");
         } else {
-            jQuery("#about").blindDown();
+            jQuery("#about").slideDown();
             jQuery("#about").addClass("visible");
+            jQuery("#about-plus").html("-");
         }
     });
 });
+
+//TODO: clean up scriptaculous files
