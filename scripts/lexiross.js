aac3cb571b7f3965b6dc64194179cefa94bb20e0
Lexi Ross <the.circ@gmail.com>
2012-07-28 20:46:39 -0400

    forgot to git add on previous commit

---
+++ scripts/lexiross.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#about-heading").click(function() {
+        if ($("#about").hasClass("visible")) {
+            $("#about").blindUp();
+            $("#about").removeClass("visible");
+        } else {
+            $("#about").blindDown();
+            $("#about").addClass("visible");
+        }
+    });
